520dac52708b82c56649db35d702b000e6661434
Tilman Kranz <tilt@linuxfoo.de>
2022-10-08 11:16:27 -0400

    move ini reader to separate package

--- example-service.go
+++ example-service.go
@@ -7,11 +7,10 @@ import(
   "io"
   "log"
   "os"
-  "path/filepath"
   "github.com/coreos/go-oidc/v3/oidc"
   "golang.org/x/net/context"
   "golang.org/x/oauth2/clientcredentials"
-  "gopkg.in/ini.v1"
+  "example-service/ini"
 )
 
 var (
@@ -22,64 +21,19 @@ var (
   usersUrl = ""
 )
 
-func readIni() {
-  ex, err := os.Executable()
-
-  if err != nil {
-    panic(err)
-  }
-
-  cfg, err := ini.Load(filepath.Join(filepath.Dir(ex), clientName + ".ini"))
-
-  if err != nil {
-    panic(err)
-  }
-
-  cs := cfg.Section(clientName)
-
-  clientID = cs.Key("clientID").String()
-
-  if clientID == "" {
-    log.Fatal(clientName + ".ini does not specify clientID")
-    os.Exit(1)
-  }
-
-  clientSecret = cs.Key("clientSecret").String()
-
-  if clientSecret == "" {
-    log.Fatal(clientName + ".ini does not specify clientSecret")
-    os.Exit(1)
-  }
-
-  providerUrl = cs.Key("providerUrl").String()
-
-  if providerUrl == "" {
-    log.Fatal(clientName + ".ini does not specify providerUrl")
-    os.Exit(1)
-  }
-
-  usersUrl = cs.Key("usersUrl").String()
-
-  if usersUrl == "" {
-    log.Fatal(clientName + ".ini does not specify usersUrl")
-    os.Exit(1)
-  }
-
-  log.Printf(
-    "Read configuration:\n" +
-    " clientID = %s\n" +
-    " clientSecret = %s\n" +
-    " providerUrl = %s\n" +
-    " usersUrl = %s\n",
-    clientID,
-    "*REDACTED*",
-    providerUrl,
-    usersUrl,
-  )
-}
-
 func main() {
-  readIni()
+  arr := []ini.Ref{
+    {"clientID", &clientID},
+    {"clientSecret", &clientSecret},
+    {"providerUrl", &providerUrl},
+    {"usersUrl", &usersUrl}}
+
+  err := ini.ReadIni(clientName, arr)
+
+  if err != nil {
+    log.Fatal()
+    os.Exit(1)
+  }
 
   ctx := context.Background()
   provider, err := oidc.NewProvider(ctx, providerUrl)
@@ -94,17 +48,6 @@ func main() {
     TokenURL: provider.Endpoint().TokenURL,
     Scopes: []string{oidc.ScopeOpenID, "profile", "email", "roles"},
   }
-
-  /* -DEBUG-
-  token, err := config.Token(ctx)
-
-  if err != nil {
-    log.Fatal(err)
-    os.Exit(1)
-  } else {
-    log.Printf("token = %s\n", token)
-  }
-  */
 
   client := config.Client(ctx)
   resp, err := client.Get(usersUrl)
---
+++ ini/reader.go
@@ -0,0 +1,49 @@
+package ini
+
+import (
+  "errors"
+  "fmt"
+  "os"
+  "path/filepath"
+  "gopkg.in/ini.v1"
+)
+
+type Ref struct {
+  Name string
+  Value *string
+}
+
+func (r Ref) readValue(cs *ini.Section) error {
+  *r.Value = cs.Key(r.Name).String()
+
+  if *r.Value == "" {
+    return errors.New(fmt.Sprintf("No value for name %s", r.Name))
+  } else {
+    return nil
+  }
+}
+
+func ReadIni(clientName string, arr []Ref) error {
+  ex, err := os.Executable()
+
+  if err != nil {
+    return err
+  }
+
+  cfg, err := ini.Load(filepath.Join(filepath.Dir(ex), clientName + ".ini"))
+
+  if err != nil {
+    return err
+  }
+
+  cs := cfg.Section(clientName)
+
+  for _, r := range arr {
+    if err := r.readValue(cs) ; err != nil {
+      return errors.New(fmt.Sprintf("Could not read value of %s", r.Name))
+    }
+  }
+
+  return nil
+}
+
